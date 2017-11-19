db07a7379ec0736cad1f2dbfba1640eb0ff9970a
unknown <mittapallimanikanta12@gmail.com>
2017-11-18 19:50:05 -0600

    first commit

---
+++ eureka-services/src/main/java/com/example/eurekaservices/EurekaServicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.eurekaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServicesApplication.class, args);
+	}
+}
